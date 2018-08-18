5224aa7cba5399b39fa2c9960dc319fc2963b56a
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2018-08-18 23:33:45 +0000

    Doctor and Patient

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def RunApp():
-    return render_template("index.html",title='My first app', content='Hello there',site='127.0.0.1:5000')
+    return render_template("patient.html",title='My first app', content='Hello there',site='127.0.0.1:5000')
 
 @app.route('/videochat')
 def VideoChat():
     return render_template("videochat.html",title='Video Chat platform')
-
 
