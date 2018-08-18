d07e22d1d20438a0130a5e2effeec4d72d6e76f4
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2018-08-18 16:14:07 +0000

    Video Chat added

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def run_app():
+def RunApp():
     return render_template("index.html",title='My first app', content='Hello there')
+
+@app.route('/videochat')
+def VideoChat():
+    return render_template("videochat.html",title='Video Chat platform')
 
 
